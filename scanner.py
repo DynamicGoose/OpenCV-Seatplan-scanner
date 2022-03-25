5a6249ba1799e029d8960f9fba797cc22c8b39d9
DynamicGoose <gezaahs@gmail.com>
2022-03-25 18:17:55 +0100

    optimization

--- scanner.py
+++ scanner.py
@@ -19,6 +19,13 @@ from cv2 import resize
 from cv2 import INTER_LINEAR
 from cv2 import addWeighted
 from cv2 import convertScaleAbs
+from cv2 import add
+from cv2 import INTER_NEAREST
+from cv2 import INTER_LANCZOS4
+from cv2 import INTER_LINEAR_EXACT
+from cv2 import INTER_TAB_SIZE2
+from cv2 import INTER_BITS
+from cv2 import INTER_CUBIC
 import numpy as np
 from cv2 import imread
 from cv2 import imshow
@@ -28,18 +35,20 @@ params = SimpleBlobDetector_Params()
 
 params.minThreshold = 0
 params.maxThreshold = 255
-params.thresholdStep = 1
+params.thresholdStep = 10
 params.filterByArea = True
-params.minArea = 200
+params.minArea = 150
 params.minRepeatability = 1
 params.filterByColor = False
 params.filterByCircularity = False
 params.filterByConvexity = True
 params.minConvexity = 0.9
-params.filterByInertia = False
+params.filterByInertia = True
+
+gap = 100
+minearea2 = 150
 
 img = imread("test.jpg",1)
-imshow("original", img)
 
 detector = SimpleBlobDetector_create(params)
 seats = detector.detect(img)
@@ -49,25 +58,23 @@ img_ws = img.copy()
 for keyPoint in seats:
     x = int(keyPoint.pt[0])
     y = int(keyPoint.pt[1])
-    circle(img_ws, (x,y), 5, (0,0,255), thickness=4, lineType=LINE_AA)
-
-imshow("plan", img_ws)
+    circle(img_ws, (x,y), 7, (0,0,255), thickness=2, lineType=LINE_AA)
 
 sum = 0
 for keyPoint in seats:
     sum = sum + keyPoint.size
-average = np.average(sum / len(seats))
+average = sum / len(seats)
 
 res_divisor = average / 15
 
-params.minArea = 150
+params.minArea = minearea2
 
 detector = SimpleBlobDetector_create(params)
 
-height = int(img_ws.shape[0] / res_divisor)
-width = int(img_ws.shape[1] / res_divisor)
+height = int(img_ws.shape[0] / res_divisor + gap)
+width = int(img_ws.shape[1] / res_divisor + gap)
 points = (int(width), int(height))
-img_rs = resize(img, points, interpolation= INTER_LINEAR)
+img_rs = resize(img, points, interpolation= INTER_LANCZOS4)
 seats = detector.detect(img_rs)
 
 f = open("output.txt", "w")
@@ -75,7 +82,7 @@ f = open("output.txt", "w")
 for keyPoint in seats:
     x = int(keyPoint.pt[0])
     y = int(keyPoint.pt[1])
-    circle(img_rs, (x,y), 5, (0,0,255), thickness=4, lineType=LINE_AA)
+    circle(img_rs, (x,y), 7, (0,0,255), thickness=1, lineType=LINE_AA)
     f.write("INSERT INTO Platz (PlanID, Stuhl, BereichID, X, Y, Bez1, Bez2, Bez3, Reflex, Xe, Ye, Sitzplatz, BestBuy, Sort, Jacket, Platz, Rotate) VALUES (138, 'Reihe 6 - Platz 1', 1, ")
     f.write(str(x))
     f.write(", ")
@@ -84,5 +91,7 @@ for keyPoint in seats:
 
 f.close()
 
-imshow("plan_final", img_rs)
+imshow("original", img)
+imshow("iteration_1", img_ws)
+imshow("iteration_2", img_rs)
 waitKey(0)

bf26ede028f35afc87a6c45f9e63fef5047ae602
DynamicGoose <gezaahs@gmail.com>
2022-03-25 11:01:16 +0100

    adjusted blob detector params

--- scanner.py
+++ scanner.py
@@ -5,18 +5,27 @@ from cv2 import SimpleBlobDetector
 from cv2 import drawKeypoints
 from cv2 import DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS
 from cv2 import SimpleBlobDetector_create
+from cv2 import SimpleBlobDetector_Params
 import numpy as np
 from cv2 import imread
 from cv2 import imshow
 from cv2 import waitKey
 
-img_gs = imread("test.jpg",0)
+params = SimpleBlobDetector_Params()
 
-detector = SimpleBlobDetector_create()
+params.minThreshold = 0
+params.maxThreshold = 255
+params.filterByArea = True
+params.minArea = 100
+params.minRepeatability = 1
 
-seats = detector.detect(img_gs)
+img = imread("test.jpg",1)
 
-img_ws = drawKeypoints(img_gs, seats, np.array([]), (0,0,255), DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+detector = SimpleBlobDetector_create(params)
+
+seats = detector.detect(img)
+
+img_ws = drawKeypoints(img, seats, np.array([]), (0,0,255), DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 imshow("seats", img_ws)
 waitKey(0)

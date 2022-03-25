d730b6538555bc62b9c62486357f606bd9cfea07
DynamicGoose <gezaahs@gmail.com>
2022-03-25 10:35:07 +0100

    first iteration of scanner

--- scanner.py
+++ scanner.py
@@ -1,11 +1,22 @@
 from email.mime import image
+from venv import create
 import cv2
+from cv2 import SimpleBlobDetector
+from cv2 import drawKeypoints
+from cv2 import DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS
+from cv2 import SimpleBlobDetector_create
+import numpy as np
 from cv2 import imread
 from cv2 import imshow
 from cv2 import waitKey
 
-img = imread("test.jpg",1)
+img_gs = imread("test.jpg",0)
 
-imshow("image",img)
+detector = SimpleBlobDetector_create()
 
+seats = detector.detect(img_gs)
+
+img_ws = drawKeypoints(img_gs, seats, np.array([]), (0,0,255), DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+imshow("seats", img_ws)
 waitKey(0)
